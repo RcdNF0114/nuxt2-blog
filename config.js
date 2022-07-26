1aba60065dd3fce7ac72b803f2ceda899680423c
RcdNF0114 <110027421+RcdNF0114@users.noreply.github.com>
2022-07-26 20:21:51 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 export default {
-  title: 'yun yu yuan', // 网站标题
-  nickName: "云与原", // 昵称
-  githubName: 'yunyuyuan', // github账户名
+  title: 'My Blog | Richard', // 网站标题
+  nickName: "RcdNF0114", // 昵称
+  githubName: 'RcdNF0114', // github账户名
   githubRepo: 'nuxt2-blog', // 勿修改
   githubBranch: 'master', // 勿修改
-  domain: 'https://blog.halberd.cn', // rss域名
-  SEO_description: '云与原的博客，书写值得书写的东西',
-  SEO_keywords: 'yunyuyuan,yunyuyuan\'s blog,云与原,云与原的博客',
+  domain: 'https://rcdnf0114.github.io', // rss域名
+  SEO_description: '我的博客',
+  SEO_keywords: 'My Blog | Richard',
   CloudflareAnalyze: "92cdf13c9c964ad1ab71c638c6fde0fc", // cloudflare的webanalyze
 }
